b120cf34704ebeee2cdf92ff5bd345eafba9e6c8
Derrick Mei <derrick.mei.168@gmail.com>
2019-12-11 01:57:13 -0600

    set up server
    need to do logger function

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
-const express = require('express');
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
+const userRouter = require("./users/userRouter");
+const postRouter = require("./posts/postRouter");
+
+// Global Middleware
 const server = express();
+server.use(express.json());
+server.use(helmet());
+// server.use(morgan('dev'));
+// server.use(logger);
 
-server.get('/', (req, res) => {
+// Routing
+server.use("/api/users", userRouter);
+server.use("/api/posts", postRouter);
+
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
+//logger logs to the console the following information about each request: request method, request url, and a timestamp
+//this middleware runs on every request made to the API
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  // console.log("req obj: ", req);
+}
 
 module.exports = server;
