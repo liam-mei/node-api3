61fcc5aa37d50f32da910afe157b47393390e8b4
Derrick Mei <derrick.mei.168@gmail.com>
2019-12-12 03:32:07 -0600

    added terminal error handler

--- server.js
+++ server.js
@@ -20,6 +20,13 @@ server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+server.use((err, req, res, next) => {
+  console.log(err)
+  res.status(500).json({
+    message: "An error occurred, please try again later."
+  })
+})
+
 //custom middleware
 //logger logs to the console the following information about each request: request method, request url, and a timestamp
 //this middleware runs on every request made to the API
